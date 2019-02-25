1bc4f13a163c3dff4cbeaffc2216b332f023bacc
Avi Waserman <aviw@axonize.com>
2019-02-25 19:53:16 +0200

    style card better :)

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import { View, StyleSheet, Image, Text } from "react-native";
 
 export default (Card = ({ avatar, fullName, tweet }) => {
   return (
-    <View style={styles.cardContainer}>
+    <View style={styles.cardContainer} >
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={{ uri: avatar }} />
       </View>
@@ -20,17 +20,21 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     padding: 10,
-    margin: 5,
-    backgroundColor: '#ededed'
+    marginBottom: 20,
+    backgroundColor: "#fff",
+    shadowColor: "#ededed",
+    shadowOffset: { width: 5, height: 7},
+    shadowOpacity: 10,
+    borderRadius: 10
   },
   imageContainer: {
-    height:50,
+    height: 50,
     width: 50,
     borderRadius: 25,
-    marginRight: 10,
+    marginRight: 10
   },
   image: {
-    height:50,
+    height: 50,
     width: 50,
     borderRadius: 25
   },
