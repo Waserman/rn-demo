f9455363a9618f78b26573b0aa737c4c80ddc3b3
Avi Waserman <aviw@axonize.com>
2019-02-25 22:05:42 +0200

    done before polish

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -1,21 +1,57 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableHighlight, AsyncStorage} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  AsyncStorage,
+  Button,
+} from "react-native";
 
 export default class SignIn extends React.Component {
 
-  submitLogin = async () => {
-    //TODO use navigation API to enter appStack
-    await AsyncStorage.setItem('k-token', 'secret-token');
-    this.props.navigation.navigate('gateway');
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    };
   }
 
+  onEmailChange = (email) => this.setState({email});
+
+  onPasswordChange = (password) => this.setState({password});
+
+  submitLogin = async () => {
+    await AsyncStorage.setItem("k-token", "secret-token");
+    this.props.navigation.navigate("gateway");
+  };
+
   render() {
+
+    const { email, password } = this.state;
+
+    const buttonEnabled = email.length && password.length;
     return (
       <View style={styles.container}>
-        <Text>SignIn page</Text>
-        <TouchableHighlight style={styles.signInButton} onPress={this.submitLogin}>
-          <Text>Sign In</Text>
-        </TouchableHighlight>
+        <Text style={styles.caption}>WELCOME BACK, CAPTAIN!</Text>
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#000"
+          placeholder="Email"
+          textContentType="emailAddress"
+          onChangeText={this.onEmailChange}
+          keyboardType="email-address" 
+        />
+        <TextInput
+          style={styles.input} 
+          placeholderTextColor="#000"
+          placeholder="Password"
+          textContentType="password"
+          onChangeText={this.onPasswordChange}
+          secureTextEntry={true}
+        />
+        <Button title="Get Connected!" disabled={!buttonEnabled} onPress={this.submitLogin}/>
       </View>
     );
   }
@@ -24,13 +60,27 @@ export default class SignIn extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    alignSelf: "stretch",
+    justifyContent: "center",
+    alignItems: 'center',
+    paddingRight: 8,
+    paddingLeft: 8,
   },
-
-  signInButton: {
-    borderColor: 'black',
-    marginTop: 10,
+  caption: {
+    width: '100%',
+    textAlign: 'center',
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginBottom: 20,
+    
+  },
+  input: {
+    width: '100%',
+    fontSize: 18,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#000',
+    padding: 5,
+    marginBottom: 15,
   }
 });
